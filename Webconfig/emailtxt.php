cde65283fe159d25be90af52da8e5acdccf3f1dd
john <johnjerome@126.com>
2014-07-04 02:39:56 +0000

    1.修改配置。 2.剔除上传文件目录的版本控制。

--- Webconfig/emailtxt.php
+++ Webconfig/emailtxt.php
@@ -1,4 +1,4 @@
 <?php
 return array ( 'regsuccess' => '亲爱的\\\'#UserName#\\\'，您好！<br>
 恭喜您注册成功,请点击下面的链接即可完成激活#LINK#<br>
-感谢您光临房产贷网络借贷平台用户中心，为您提供优质的服务！<br> <div style=\\"text-align:right\\"房产贷网络借贷平台   敬启</div>', 'safecode' => '你好，更改安全中心的邮件验证码是\\"#CODE#\\"', 'changephone' => '你好，你申请更换新手机的安全码为\\"#CODE#\\"', 'verifysuccess' => '借款申请审核通过', 'getpass' => '您好，您申请了密码找回，请点击验证链接#LINK#进行密码找回的下一步操作', ); ?>
+感谢您光临圆创贷网络借贷平台用户中心，为您提供优质的服务！<br> <div style=\\"text-align:right\\"圆创贷网络借贷平台   敬启</div>', 'safecode' => '你好，更改安全中心的邮件验证码是\\"#CODE#\\"', 'changephone' => '你好，你申请更换新手机的安全码为\\"#CODE#\\"', 'verifysuccess' => '借款申请审核通过', 'getpass' => '您好，您申请了密码找回，请点击验证链接#LINK#进行密码找回的下一步操作', ); ?>
--- Webconfig/msgtxt.php
+++ Webconfig/msgtxt.php
@@ -1,2 +1,2 @@
 <?php
-return array ( 'regsuccess' => '\\\'#UserName#\\\'你好，恭喜您注册成功【房产贷】', 'verifysuccess' => '借款申请审核通过', ); ?>
+return array ( 'regsuccess' => '\\\'#UserName#\\\'你好，恭喜您注册成功【圆创贷】', 'verifysuccess' => '借款申请审核通过', ); ?>
