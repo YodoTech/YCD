368e5c257fa015d3d2571af826444a5b04b44e51
john <johnjerome@126.com>
2014-09-20 08:07:39 +0000

    修改“返回顶部”图片

--- Style/H/js/backtotop.js
+++ Style/H/js/backtotop.js
@@ -2,7 +2,7 @@ var scrolltotop={
     //startline: Integer. Number of pixels from top of doc scrollbar is scrolled before showing control   
     //scrollto: Keyword (Integer, or "Scroll_to_Element_ID"). How far to scroll document up when control is clicked on (0=top).   
     setting: {startline:100, scrollto: 0, scrollduration:1000, fadeduration:[500, 100]},   
-    controlHTML: '<img src="/Public/images/btn_top.gif" style="width:36px;height:36px" />', //HTML for control, which is auto wrapped in DIV w/ ID="topcontrol"   
+    controlHTML: '<img src="/Public/images/topq.jpg" style="width:43px;height:43px" />', //HTML for control, which is auto wrapped in DIV w/ ID="topcontrol"   
     controlattrs: {offsetx:50, offsety:50}, //offset of control relative to right/ bottom of window corner   
     anchorkeyword: '#top', //Enter href value of HTML anchors on the page that should also act as "Scroll Up" links   
   
