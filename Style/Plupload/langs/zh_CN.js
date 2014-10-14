ce16a7dc3ec86956c0de0c0ef4bb336d2d0e2eda
john <johnjerome@126.com>
2014-10-14 03:46:28 +0000

    1、上传Plupload插件； 2、前台借款资料上传，后台借款资料上传/审核； 3、后台借款资料审核通过后即可生成借款申请；

---
+++ Style/Plupload/langs/zh_CN.js
@@ -0,0 +1,15 @@
+plupload.addI18n({
+	'Select files' : '选择上传文件',
+	'Add files to the upload queue and click the start button.' : '可以添加多个文件',
+	'Filename' : '文件名',
+	'Status' : '状态',
+	'Size' : '大小',
+	'Add files' : '添加文件',
+	'Stop current upload' : '停止删除',
+	'Start uploading queue' : '开始上传',
+	'Drag files here.' : '请拖入文件或点击"添加文件"',
+	'Start upload': '开始上传',
+	'Uploaded %d/%d files': '已上传 %d/%d 文件',
+	'Error: Invalid file extension':'无效的文件格式',
+	'Error: File too large':'错误：文件大小超过设定'
+});
