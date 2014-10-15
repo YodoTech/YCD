b120da93cae79be3ecbac2f0ac41800dfda96748
john <johnjerome@126.com>
2014-10-15 06:46:28 +0000

    1、个人中心添加“自动投标”链接； 2、修改部分页面文字；

--- App/Lib/Action/Member/AutoborrowAction.class.php
+++ App/Lib/Action/Member/AutoborrowAction.class.php
@@ -2,12 +2,18 @@
 // 本类由系统自动生成，仅供测试用途
 class AutoborrowAction extends MCommonAction {
 
-    public function index(){
-		$this->assign("list",M('auto_borrow')->where("uid={$this->uid}")->limit(3)->select());
+	public function index() {
 		$this->display();
+	}
+
+    public function summary() {
+		$this->assign('list', M('auto_borrow')->where("uid={$this->uid}")->limit(3)->select());
+
+		$json['content'] = $this->fetch();
+		exit(json_encode($json));
     }
 	
-	public function add(){
+	public function add() {
 		$Bconfig = require C("APP_ROOT")."Conf/borrow_config.php";
 		
 		$borrow_duration = explode("|",$this->glo['borrow_duration']);
@@ -21,10 +27,12 @@ class AutoborrowAction extends MCommonAction {
 		$this->assign("repayment_type",$Bconfig['REPAYMENT_TYPE']);	
 		$this->assign("borrow_type",$Bconfig['BORROW_TYPE']);	
 		$this->assign("xmembertype",C('XMEMBER_TYPE'));	
-		$this->display();
+		
+		$json['content'] = $this->fetch();
+		exit(json_encode($json));
 	}
 	
-	public function doadd(){
+	public function doadd() {
 		$model=D('Auto_borrow');
 		$savedata = textPost($_POST);
 		$savedata['uid'] = $this->uid;
@@ -43,13 +51,13 @@ class AutoborrowAction extends MCommonAction {
 		if($result = $model->add()) {
 			$id=$model->getLastInsID();  
 			addInnerMsg($this->uid,"新增自动投标","您新添加了第{$id}号自动投标，新增人id:{$this->uid}");
-			$this->success("添加成功",__URL__."/index.html");
+			$this->success("添加成功",__URL__);
         } else {
 			$this->error("添加失败");
         }
 	}
 	
-	public function edit(){
+	public function edit() {
 		$id=intval($_GET['id']);
 		$map['uid'] = $this->uid;
 		$map['id'] = $id;
@@ -70,10 +78,12 @@ class AutoborrowAction extends MCommonAction {
 		$this->assign("repayment_type",$Bconfig['REPAYMENT_TYPE']);	
 		$this->assign("borrow_type",$Bconfig['BORROW_TYPE']);	
 		$this->assign("xmembertype",C('XMEMBER_TYPE'));	
-		$this->display();
+		
+		$json['content'] = $this->fetch();
+		exit(json_encode($json));
 	}
 	
-	public function doedit(){
+	public function doedit() {
 		$model=D('Auto_borrow');
 		$_X=array('my_friend','not_black','borrow_credit_status','apr_status','award_status');
 		foreach($_X as $ve){
@@ -96,22 +106,21 @@ class AutoborrowAction extends MCommonAction {
 		if($result = $model->save()) {
 			$id = $_POST['id'];
 			addInnerMsg($this->uid,"编辑自动投标参数","您对第{$id}号自动投标参数进行了调整，修改人id:{$this->uid}");
-			$this->success("编辑成功",__URL__."/index.html");
+			$this->success("编辑成功",__URL__);
         } else {
 			$this->error("编辑失败");
         }
 	}
 	
-	public function del(){
+	public function del() {
 		$id=intval($_GET['id']);
 		$map['uid'] = $this->uid;
 		$map['id'] = $id;
 		$vo = M('auto_borrow')->where($map)->delete();
 		if(!$vo) $this->error("删除失败，请重试");
-		else{
+		else {
 		   addInnerMsg($this->uid,"删除自动投标","您删除了第{$id}号自动投标的设置记录,删除人id:{$this->uid}");
-		  $this->success("删除成功",__URL__."/index.html");
+		  $this->success("删除成功",__URL__);
 		}
 	}
-
 }
